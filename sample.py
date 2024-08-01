c9106714357689d7a1b3ac58bfc37d32624ad65d
Zhenbang Du <50868745+ZhenbangDu@users.noreply.github.com>
2024-08-01 11:22:05 +0800

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,36 @@
+import argparse
+from pipeline.pipeline_sample import T2I
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pipeline', type=str, default='T2I')
+    parser.add_argument('--base_model_path', type=str, default='runwayml/stable-diffusion-v1-5')
+    parser.add_argument('--controlnet_model_path', type=str, default='lllyasviel/sd-controlnet-canny')
+    parser.add_argument('--lora_model_path', type=str, default=None)
+    parser.add_argument('--lora_scale', type=float, default=0.8)
+    parser.add_argument('--batch_size', type=int, default=50)
+    parser.add_argument('--clip_skip', default=None)
+    parser.add_argument('--negative_prompt', type=str, default="") 
+    parser.add_argument('--guidance_scale', type=float, default=7.5)
+    parser.add_argument('--eta', type=float, default=0.0)
+    parser.add_argument('--controlnet_conditioning_scale', type=float, default=1.0)
+    parser.add_argument('--width', type=int, default=512)
+    parser.add_argument('--height', type=int, default=512)
+    parser.add_argument('--image_scale', type=float, default=0.7)
+    parser.add_argument('--num_inference_steps', type=int, default=40)
+    parser.add_argument('--iterations', type=int, default=5)
+    parser.add_argument('--low_threshold', type=int, default=100)
+    parser.add_argument('--high_threshold', type=int, default=200)
+    parser.add_argument('--keep_loc', action='store_true')
+    parser.add_argument('--seed', default=None)
+    parser.add_argument('--save_concat', default=False, action='store_true')
+    parser.add_argument('--sampler_name', type=str, choices=['Euler a', 'DPM++ SDE Karras', 'DDIM'], default='DDIM')
+    parser.add_argument('--save_path', type=str, default='./results')
+    parser.add_argument('--data_path', type=str, default="./samples")
+    parser.add_argument('--config', type=str, default="./config/config.json")
+    return parser.parse_args()
+
+if __name__=="__main__":
+    args = get_parser()
+    pipe = T2I(args)
+    pipe.inference(args)
