fd3e3b188360febdffded7a73f1bbe345641f2f6
Zhenbang Du <50868745+ZhenbangDu@users.noreply.github.com>
2024-08-01 11:27:57 +0800

    Create sample.sh

---
+++ sample.sh
@@ -0,0 +1,11 @@
+echo "----------sampling----------"
+
+python sample.py \
+--base_model_path "digiplay/majicMIX_realistic_v7" \
+--controlnet_model_path  "lllyasviel/control_v11p_sd15_canny" \
+--batch_size 10 \
+--sampler_name 'DDIM'  \
+--num_inference_steps 40 \
+--config ./config/config.json  \
+--data_path  ./samples \
+--save_path ./outputs
